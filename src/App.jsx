827a1797971dfa364469f3f1c432b7807fd5bed1
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-23 21:18:13 -0300

    instalo libreria bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-     
+      <h1>Administrador pacientes veterinaria</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
