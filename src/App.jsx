e18af0523e513261dabcb09204c6c324b2b3f74e
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-24 23:17:17 -0300

    creo es state , agrego localstorage y agrego el componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Formulario from "./componenst/Formulario";
+import { useEffect, useState } from "react";
 
 function App() {
+  let citaLocalStorage = JSON.parse(localStorage.getItem("citas")) || [];
+  const [citasApp, setCitasApp] = useState(citaLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("citas", JSON.stringify(citasApp));
+  }, [citasApp]);
+
+  const crearCitas = (cita) => {
+    setCitasApp([...citasApp, cita]);
+  };
   return (
     <>
       <div>
         <h1 className="tex-danger">Administrador pacientes veterinaria</h1>
       </div>{" "}
-      <Formulario></Formulario>
+      <Formulario crearCitas={crearCitas}></Formulario>
     </>
   );
 }
--- src/componenst/Formulario.jsx
+++ src/componenst/Formulario.jsx
@@ -1,30 +1,70 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-const Formulario = () => {
+const Formulario = ({ crearCitas }) => {
+  const [cita, setCita] = useState({
+    nombreDeMascota: "",
+    nombreDeDueño: "",
+    hora: "",
+    fecha: "",
+    sintomas: "",
+  });
+
+  const valorInputs = (e) => {
+    setCita({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <Form className="container">
       <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
         <Form.Label> Nombre de mascota</Form.Label>
-        <Form.Control type="text" placeholder="Nombre de mascota" />
+        <Form.Control
+          type="text"
+          name="nombreDeMascota"
+          value={cita.nombreDeMascota}
+          placeholder="Nombre de mascota"
+        />
       </Form.Group>
       <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
         <Form.Label> Nombre de dueño</Form.Label>
-        <Form.Control type="text" placeholder="Nombre de dueño" />
+        <Form.Control
+          type="text"
+          name="nombreDeDueño"
+          value={cita.nombreDeDueño}
+          placeholder="Nombre de dueño"
+        />
       </Form.Group>
-      <div className="d-flex justify-content-between">
-        <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-          <Form.Label> fecha</Form.Label>
-          <Form.Control type="namber" placeholder="fecha" />
+      <div className="row ">
+        <Form.Group className="mb-3 d-flex col-6" controlId="formBasicEmail">
+          <Form.Label className="mt-2 me-2"> fecha</Form.Label>
+          <Form.Control
+            type="number"
+            name="fecha"
+            value={cita.fecha}
+            placeholder="fecha"
+          />
         </Form.Group>
-        <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-          <Form.Label> hora</Form.Label>
-          <Form.Control type="namber" placeholder="namber" />
+        <Form.Group className="mb-3 d-flex col-6" controlId="formBasicEmail">
+          <Form.Label className="mt-2 me-2"> hora</Form.Label>
+          <Form.Control
+            type="number"
+            name="hora"
+            value={cita.hora}
+            placeholder="namber"
+          />
         </Form.Group>
       </div>
       <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-        <Form.Label> Sintomas</Form.Label>
-        <Form.Control type="text" placeholder="Describir sintomas" />
+        <Form.Label className="mt-2 me-2"> Sintomas</Form.Label>
+        <Form.Control
+          type="text"
+          name="sintomas"
+          value={cita.sintomas}
+          placeholder="Describir sintomas"
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit">
